f852f3b31f4eabdf2b5645e327ef535ec698e26c
VintoN <vinton.mail@gmail.com>
2020-05-03 23:45:27 +0300

    conscience: refactored and finished part 5 on rx5

--- dist/5th_rx5.js
+++ dist/5th_rx5.js
@@ -3,22 +3,16 @@ console.log('5th_rx5.js is loading...')
 const Rx = window['Rx']
 console.log('Rx is:', Rx)
 
-// renamed request Stream, because that's one we do on app startup:
-let startupRequestStream = Rx.Observable.of('https://api.github.com/users')
-startupRequestStream.subscribe(url => console.log('startup request url will be:', url))
-
 const refreshBtn = document.querySelector('.refresh-btn')
 
-// create another Stream, for refresh button click events:
+// create Stream, for refresh button click events:
 const refreshClickStream = Rx.Observable.fromEvent(refreshBtn, 'click')
 refreshClickStream.subscribe(evt => console.log('Refresh click fired!'))
 
-const refreshRequestStream = refreshClickStream
+// map clicks to url stream and adds initial value for startup:
+const requestStream = refreshClickStream
     .map(() => `https://api.github.com/users?since=${Math.floor(Math.random() * 500)}`)
-refreshRequestStream.subscribe(url => console.log('refresh request url will be a random url, for example:',url))
-
-// merging above 2 Streams into one:
-const requestStream = startupRequestStream.merge(refreshRequestStream)
+    .startWith('https://api.github.com/users')
 requestStream.subscribe(response => console.log('merged request stream response:', response))
 
 // use 3rd tryish tyle from 4th example...
