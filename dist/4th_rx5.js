d0fd72aa0fddef8a2ac9321f22095891ae27de24
VintoN <vinton.mail@gmail.com>
2020-05-03 22:31:45 +0300

    conscience: finished 4th part

--- dist/4th_rx5.js
+++ dist/4th_rx5.js
@@ -4,6 +4,8 @@ const Rx = window['Rx']
 console.log('Rx is:', Rx)
 
 let requestStream = Rx.Observable.of('https://api.github.com/users')
+
+// 1st try, ugly callback-hell like:
 requestStream.subscribe(url => {
     let responseStream = Rx.Observable.create(observer => {
         fetch(url)
@@ -16,10 +18,12 @@ requestStream.subscribe(url => {
     responseStream.subscribe(response => console.log('Received response:', response))
 })
 
+// 2nd try, uses valid stream methods, no folding, but got metaStream of promises:
 let responseMetaStream = requestStream
     .map(url => Rx.Observable.fromPromise(fetch(url).then(response => response.json())))
 responseMetaStream.subscribe(dataStream => console.log('dataStream:', dataStream))
 
+// 3rd try, uses flatMap, for flattening stream of promises-like streams...
 let outerResponseStream = requestStream
     .flatMap(url => Rx.Observable.fromPromise(fetch(url).then(response => response.json())))
 outerResponseStream.subscribe(response => {
--- dist/4th_rx6.js
+++ dist/4th_rx6.js
@@ -2,10 +2,12 @@ console.log('4nd_rx6.js is loading...')
 
 // import * as Rx from 'https://dev.jspm.io/rxjs@6/_esm2015'
 // import * as Op from 'https://dev.jspm.io/rxjs@6/_esm2015/operators'
-import { Observable, of, fromEvent } from 'https://dev.jspm.io/rxjs@6/_esm2015'
-import { buffer, map, filter, debounceTime } from 'https://dev.jspm.io/rxjs@6/_esm2015/operators'
+import { Observable, of, fromEvent, from } from 'https://dev.jspm.io/rxjs@6/_esm2015'
+import { buffer, map, flatMap, filter, debounceTime } from 'https://dev.jspm.io/rxjs@6/_esm2015/operators'
 
 let requestStream = of('https://api.github.com/users')
+
+// 1st try, ugly callback-hell like:
 requestStream.subscribe(url => {
     let responseStream = Observable.create(observer => {
         fetch(url)
@@ -17,5 +19,20 @@ requestStream.subscribe(url => {
 
     responseStream.subscribe(response => console.log('Response was:', response))
 })
+
+// 2nd try, uses valid stream methods, no folding, but got metaStream of promises:
+let responseMetaStream = requestStream
+    .pipe(
+        map(url => from(fetch(url).then(response => response.json())))
+    )
+responseMetaStream.subscribe(promiseStream => console.log('promiseStream is:', promiseStream))
+
+// 3rd try, uses flatMap, for flattening stream of promises-like streams...
+let outerResponseStream = requestStream
+    .pipe(
+        flatMap(url => from(fetch(url).then(response => response.json())))
+    )
+outerResponseStream.subscribe(jsonData => console.log('outer response data:', jsonData))
+
 
 console.log('4nd_rx6.js was loaded')
