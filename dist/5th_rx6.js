4d32694274c2bab4c23f4593f1cc8aef1d1b7c7a
VintoN <vinton.mail@gmail.com>
2020-05-03 23:54:36 +0300

    conscience: done part 5 with rxjs 6

--- dist/5th_rx6.js
+++ dist/5th_rx6.js
@@ -2,17 +2,34 @@ console.log('5nd_rx6.js is loading...')
 
 // import * as Rx from 'https://dev.jspm.io/rxjs@6/_esm2015'
 // import * as Op from 'https://dev.jspm.io/rxjs@6/_esm2015/operators'
-import { Observable, of, fromEvent, from } from 'https://dev.jspm.io/rxjs@6/_esm2015'
+import { Observable, of, fromEvent, from, merge } from 'https://dev.jspm.io/rxjs@6/_esm2015'
 import { buffer, map, flatMap, filter, debounceTime } from 'https://dev.jspm.io/rxjs@6/_esm2015/operators'
 
-let requestStream = of('https://api.github.com/users')
+let startupRequestStream = of('https://api.github.com/users')
+startupRequestStream.subscribe(url => console.log('startup request url will be:', url))
+
+const refreshBtn = document.querySelector('.refresh-btn')
+
+// create another Stream, for refresh button click events:
+const refreshClickStream = fromEvent(refreshBtn, 'click')
+refreshClickStream.subscribe(evt => console.log('Refresh click fired!'))
+
+const refreshRequestStream = refreshClickStream
+    .pipe(
+        map(() => `https://api.github.com/users?since=${Math.floor(Math.random() * 500)}`)
+    )
+refreshRequestStream.subscribe(url => console.log('refresh request url will be a random url, for example:',url))
+
+// merging above 2 Streams into one:
+const requestStream = merge(startupRequestStream, refreshRequestStream)
+requestStream.subscribe(response => console.log('merged request stream response:', response))
 
 // 3rd try, uses flatMap, for flattening stream of promises-like streams...
-let outerResponseStream = requestStream
+let responseStream = requestStream
     .pipe(
         flatMap(url => from(fetch(url).then(response => response.json())))
     )
-outerResponseStream.subscribe(jsonData => console.log('outer response data:', jsonData))
+responseStream.subscribe(jsonData => console.log('response json data:', jsonData))
 
 
 console.log('5nd_rx6.js was loaded')
